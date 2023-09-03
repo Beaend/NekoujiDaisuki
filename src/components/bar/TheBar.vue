4703d5947a6042707ba04d7ba600c7400a324063
Beaend <beaend@ya.ru>
2023-09-03 19:57:38 +0300

    Add global styles

--- src/components/bar/TheBar.vue
+++ src/components/bar/TheBar.vue
@@ -25,5 +25,18 @@ const genres = [[1, 'Приключения'], [2, 'Повседневность
   </nav>
 </template>
 
-<style scoped>
+<style>
+:root {
+  --green: #1dff1d;
+  --blue: rgb(63,191,256);
+  --gold: #FFD700;
+
+  --bar_bg: rgb(31,35,39);
+  --bar_bg_2: rgb(25, 28, 31);
+  --bar_bg_3: rgb(39,43,47);
+  --bar_color: rgb(223,223,223);
+  --bar_color_hide: rgb(191,199,207);
+  --bar_color_2: #becde2;
+  --bar_active: rgb(63,127,191);
+}
 </style>
