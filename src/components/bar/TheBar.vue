7c47bdd2fa864cc10356d4b7c8213a35c12e0b38
Beaend <beaend@ya.ru>
2023-09-03 23:52:10 +0300

    Transfer data to barStore

--- src/components/bar/TheBar.vue
+++ src/components/bar/TheBar.vue
@@ -6,22 +6,12 @@ import BarTop from '~/components/bar/BarTop.vue'
 defineOptions({
   name: 'TheBar',
 })
-
-const years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009]
-const genres = [[1, 'Приключения'], [2, 'Повседневность'], [3, 'Комедия'], [4, 'Фантастика'], [5, 'Экшен'],
-  [6, 'Драма'], [13, 'Романтика']]
 </script>
 
 <template>
   <nav>
-    <BarTop
-      :years=years
-      :genres=genres
-    />
-    <!--    <BarBottom -->
-    <!--      :years=years -->
-    <!--      :genres=genres -->
-    <!--    /> -->
+    <BarTop />
+    <!--    <BarBottom /> -->
   </nav>
 </template>
 
