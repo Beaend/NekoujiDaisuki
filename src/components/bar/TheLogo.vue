6283bb6b37bb00a31388ffef7e8aab64a33eeda5
Beaend <beaend@ya.ru>
2023-09-03 19:03:10 +0300

    Add Logo

---
+++ src/components/bar/TheLogo.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: 'TheLogo',
+}
+</script>
+
+<template>
+  <router-link id="logo" to="/">
+    <img title="Nekouji Daisuki" src="/favicon.svg" alt="Nekouji Daisuki">
+    <span id="logo-text">Daisuki</span>
+  </router-link>
+</template>
+
+<style scoped>
+#logo {
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  padding: 0 30px;
+  img {
+    height: 50px;
+    width: 50px;
+  }
+}
+#logo-text {
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 50px;
+  margin-top: 2px;
+  color: $bar_color;
+  transform: skew(-10deg);
+}
+</style>
