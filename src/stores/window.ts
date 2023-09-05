2fca34fd766745abaa67abbb32ec42922e103434
Cadregich2 <theloyworkmail@gmail.com>
2023-09-05 11:54:39 +0300

    Added getters to get information about the device

--- src/stores/window.ts
+++ src/stores/window.ts
@@ -8,4 +8,25 @@ export const windowStore = defineStore({
       height: 0,
     },
   }),
+  getters: {
+    getBarType(): string {
+      if (this.screenSize.width > 800)
+        return 'top'
+      else
+        return 'bottom'
+    },
+    getDeviceType(): string {
+      const screenWidth: number = this.screenSize.width
+      let deviceType: string = 'unknown'
+      if (screenWidth <= 380)
+        deviceType = 'tiny'
+      else if (screenWidth <= 480)
+        deviceType = 'smartphone'
+      else if (screenWidth <= 800)
+        deviceType = 'tablet'
+      else if (screenWidth > 800)
+        return 'pc'
+      return deviceType
+    },
+  },
 })
