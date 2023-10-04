63dcc16abbc39383d2228d67a4be867ae971873c
Beaend <beaend@ya.ru>
2023-10-04 22:44:53 +0300

    Add loading (blank) card

---
+++ src/components/cards/CardBlank.vue
@@ -0,0 +1,73 @@
+<script setup>
+</script>
+
+<template>
+  <div class="card">
+    <div class="left">
+      <img src="/img/loading%20a4.png" :alt="$t('loadingPage')">
+    </div>
+    <div class="right">
+      <div class="title blankText" />
+      <div class="description blankText" />
+      <div class="description blankText" />
+      <div class="description blankText" />
+      <div class="description blankText" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.left {
+  background: linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab);
+  background-size: 400% 400%;
+  animation: bg_gradient 10s ease infinite;
+  height: 100%;
+}
+
+@keyframes bg_gradient {
+  0% {
+    background-position: 0 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0 50%;
+  }
+}
+
+.right {
+  padding: 20px;
+  .title {
+    width: 80%;
+    height: 40px;
+    margin-bottom: 25px;
+  }
+  .description {
+    width: 100%;
+    height: 18px;
+    margin: 5px 0;
+  }
+  .description:last-child {
+    width: 70%;
+  }
+}
+
+.blankText {
+  background: linear-gradient(-135deg, rgb(31,33,35), rgb(63,67,71), rgb(127,133,139), rgb(63,67,71));
+  background-size: 400% 400%;
+  animation: text_gradient 10s ease infinite;
+}
+
+@keyframes text_gradient {
+  0% {
+    background-position: 100% 50%;
+  }
+  50% {
+    background-position: 0 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
+}
+</style>
