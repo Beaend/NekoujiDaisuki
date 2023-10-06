911171885e4117daf439d70b72388d1986054588
Beaend <beaend@ya.ru>
2023-10-06 10:04:24 +0300

    Search - add cookies - add sorting

--- src/pages/search/[searchValue].vue
+++ src/pages/search/[searchValue].vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { onMounted } from 'vue-demi'
+import Sorting from '~/components/Sorting.vue'
 import Api from '~/services/ApiCall.js'
+import { userCookies } from '~/stores/cookies'
 
 defineOptions({
   name: 'SearchPage',
@@ -11,6 +12,7 @@ const route = useRoute()
 const anime = ref([])
 const manga = ref([])
 const books = ref([])
+const cookies = userCookies()
 
 function getList(value) {
   Api.search(value)
@@ -19,39 +21,56 @@ function getList(value) {
       manga.value = response.data.manga
       books.value = response.data.books
     })
-    .catch((e) => {
-      console.log(e)
-    })
 }
 
-onMounted(() => {
+onBeforeMount(() => {
   getList(route.params.searchValue)
 })
 onBeforeRouteUpdate(async (to, from) => {
-  if (to.params.searchValue !== from.params.searchValue) {
+  if (to.params.searchValue !== from.params.searchValue)
     getList(to.params.searchValue)
-  }
 })
 </script>
 
 <template>
   <main>
     <h1>{{ $t('search') }}: {{ route.params.searchValue }}</h1>
-    <h2 v-if="anime.length > 0">Аниме</h2>
+    <h2 v-if="anime.length > 0">
+      {{ $t('anime') }}
+    </h2>
+    <Sorting
+      v-if="anime.length > 0"
+      v-model="anime" :raw-data="anime" :settings="cookies.sorting.index"
+      :hide-years="true"
+    />
     <div v-if="anime.length > 0" class="shelf">
       <CardAnime
         v-for="item in anime" :key="item.title"
         :anime="item"
       />
     </div>
-    <h2 v-if="manga.length > 0">Манга</h2>
+    <h2 v-if="manga.length > 0">
+      {{ $t('comics.manga') }}
+    </h2>
+    <Sorting
+      v-if="manga.length > 0"
+      v-model="manga" :raw-data="manga" :settings="cookies.sorting.index"
+      :hide-years="true"
+    />
     <div v-if="manga.length > 0" class="shelf">
       <CardManga
         v-for="item in manga" :key="item.title"
         :manga="item"
       />
     </div>
-    <h2 v-if="books.length > 0">Ранобе</h2>
+    <h2 v-if="books.length > 0">
+      {{ $t('ranobe') }}
+    </h2>
+    <Sorting
+      v-if="books.length > 0"
+      v-model="books" :raw-data="books" :settings="cookies.sorting.index"
+      :hide-years="true"
+    />
     <div v-if="books.length > 0" class="shelf">
       <CardRanobe
         v-for="item in books" :key="item.title"
