c83e1b8a12b23e33d019f201d4ea225bdc6f425a
Beaend <beaend@ya.ru>
2023-11-03 08:58:28 +0300

    [animeStudio].vue - remove sorting component - add pre sorting - add loading - edit showcase

--- src/pages/studio/[animeStudio].vue
+++ src/pages/studio/[animeStudio].vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
-import Sorting from '~/components/Sorting.vue'
 import Api from '~/services/ApiCall.js'
-import { userCookies } from '~/stores/cookies'
+import { Sorting } from '~/services/Sorting.ts'
 
 defineOptions({
   name: 'StudioPage',
 })
 
 const route = useRoute()
+const { t } = useI18n()
 
-const anime = ref([])
-const cookies = userCookies()
+const rawData = ref([])
+const anime = ref({})
+const ready = ref(false)
+const sortedYears = ref([])
 
-function getList(name) {
-  Api.studio(name)
+async function getList(name) {
+  await Api.studio(name)
     .then((response) => {
-      anime.value = response.data
+      rawData.value = response.data
     })
+  const sort = new Sorting(rawData.value)
+  sort.byName('title')
+  sort.byQuality()
+  for (const i in rawData.value) {
+    if (!anime.value[rawData.value[i].year])
+      anime.value[rawData.value[i].year] = []
+    anime.value[rawData.value[i].year].push(rawData.value[i])
+  }
+  sortedYears.value = Object.keys(anime.value).sort().reverse()
+  ready.value = true
 }
 
 onBeforeMount(() => {
@@ -30,16 +42,18 @@ onBeforeRouteUpdate(async (to, from) => {
 
 <template>
   <main>
-    <h1>{{ $t('studio') }}: {{ route.params.animeStudio }}</h1>
-    <Sorting
-      v-if="anime.length > 0"
-      v-model="anime" :raw-data="anime" :settings="cookies.sorting.index"
-    />
-    <div v-if="anime.length > 0" class="shelf">
-      <CardAnime
-        v-for="item in anime" :key="item.title"
-        :anime="item"
-      />
+    <h1>{{ t('studio') }}: {{ route.params.animeStudio }}</h1>
+    <div v-if="!ready" class="shelf">
+      <CardBlank v-for="i in [1, 2, 3]" :key="i" />
     </div>
+    <template v-for="year in sortedYears" :key="year">
+      <h2>{{year}}</h2>
+      <div v-if="ready" class="shelf">
+        <CardAnime
+          v-for="item in anime[year]" :key="item.title"
+          :anime="item"
+        />
+      </div>
+    </template>
   </main>
 </template>
