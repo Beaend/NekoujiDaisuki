15bf377fdf190f0f611b9db50f5d17000d6b5a07
Beaend <beaend@ya.ru>
2023-11-15 03:13:35 +0300

    upd

--- src/stores/bar.ts
+++ src/stores/bar.ts
@@ -6,8 +6,16 @@ export const barStore = defineStore({
     searchFocused: false,
     searchValue: '',
     years: [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009],
-    genres: [[1, 'Приключения'], [2, 'Повседневность'], [3, 'Комедия'], [4, 'Фантастика'], [5, 'Экшен'],
-      [6, 'Драма'], [13, 'Романтика']],
+    genres: [
+      { id: 1, ru: 'Приключения', en: 'Adventure' },
+      { id: 2, ru: 'Повседневность', en: 'Slice of life' },
+      { id: 3, ru: 'Комедия', en: 'Comedy' },
+      { id: 4, ru: 'Фэнтези', en: 'Fantasy' },
+      { id: 12, ru: 'Науч. фантастика', en: 'Sci-Fi' },
+      { id: 5, ru: 'Экшен', en: 'Action' },
+      { id: 6, ru: 'Драма', en: 'Drama' },
+      { id: 13, ru: 'Романтика', en: 'Romance' },
+    ],
   }),
   actions: {
     switchSearch() {
