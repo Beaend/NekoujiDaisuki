c50308e7ec1172e9670e2f6d07588109ed369ede
Beaend <beaend@ya.ru>
2023-11-16 03:52:55 +0300

    upd - less api call - add description to genre

--- src/pages/anime/genre/[genre].vue
+++ src/pages/anime/genre/[genre].vue
@@ -12,26 +12,41 @@ const route = useRoute()
 
 const anime = ref([])
 const genre = ref(String)
+const genres = ref([])
+
 function getAnimeByGenre(genre) {
   Api.getAnimeGenre(genre)
     .then((response) => {
+      anime.value = []
       anime.value.push(...response.data[0])
       anime.value.push(...response.data[1])
     })
 }
-function getGenreName() {
-  Api.getGenres()
+async function getGenres() {
+  await Api.getGenres()
     .then((response) => {
-      for (let g = 0; g < response.data.length; g++) {
-        if (response.data[g].id === Number(route.params.genre))
-          genre.value = response.data[g].en
-      }
+      genres.value = response.data
     })
 }
 
-onBeforeMount(() => {
+function getGenreName(genreName) {
+  for (let g = 0; g < genres.value.length; g++) {
+    if (genres.value[g].id === Number(genreName))
+      genre.value = genres.value[g]
+  }
+}
+
+onBeforeMount(async () => {
   getAnimeByGenre(route.params.genre)
-  getGenreName()
+  await getGenres()
+  getGenreName(route.params.genre)
+})
+
+onBeforeRouteUpdate(async (to, from) => {
+  if (to.params.genre !== from.params.genre) {
+    getAnimeByGenre(to.params.genre)
+    getGenreName(to.params.genre)
+  }
 })
 
 const cookies = userCookies()
@@ -39,7 +54,20 @@ const cookies = userCookies()
 
 <template>
   <main>
-    <h1>{{ t('anime') }} {{ t(`genres.${genre}`) }}</h1>
+    <h1>
+      {{ t('anime') }}
+      <span v-if="!genre[`${$i18n.locale}_description`]">
+        {{ genre[$i18n.locale] }}
+      </span>
+      <span v-else style="cursor: help;">
+        {{ genre[$i18n.locale] }}
+        <div class="genre-description">
+          <p v-for="g in genre[`${$i18n.locale}_description`]" :key="g.id">
+            {{ g }}
+          </p>
+        </div>
+      </span>
+    </h1>
     <Sorting
       v-model="anime" :raw-data="anime" :settings="cookies.sorting.index"
     />
@@ -48,3 +76,26 @@ const cookies = userCookies()
     </div>
   </main>
 </template>
+
+<style>
+h1 {
+  position: relative;
+  cursor: default;
+  user-select: none;
+}
+.genre-description {
+  position: absolute;
+  display: none;
+  font-size: 1rem;
+  font-weight: normal;
+  background: var(--bar_bg);
+  padding: 10px;
+  border-radius: 8px;
+  left: 50%;
+  transform: translate(-50%);
+  z-index: 2;
+}
+h1:hover .genre-description {
+  display: block;
+}
+</style>
