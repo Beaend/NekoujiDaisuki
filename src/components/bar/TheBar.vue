3aadc389c823aeb5ecc3ee0ee261c96a672c4b25
Beaend <beaend@ya.ru>
2023-11-16 06:11:21 +0300

    Add mobile view

--- src/components/bar/TheBar.vue
+++ src/components/bar/TheBar.vue
@@ -11,15 +11,17 @@ defineOptions({
 <template>
   <nav>
     <BarTop />
-    <!--    <BarBottom /> -->
+    <BarBottom />
   </nav>
 </template>
 
 <style>
 :root {
   --green: #1dff1d;
-  --blue: rgb(63,191,256);
+  --blue: rgb(63,191,255);
   --gold: #FFD700;
+  --blue-passive: rgb(63, 127, 224);
+  --blue-active: rgb(63, 223, 255);
 
   --bar_bg: rgb(31,35,39);
   --bar_bg_2: rgb(25, 28, 31);
