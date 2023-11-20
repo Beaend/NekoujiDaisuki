6d9124c2040ec3fc4d7a9012967ab994c5c63100
Beaend <beaend@ya.ru>
2023-11-20 14:13:17 +0300

    add loading

--- src/pages/ranobe.vue
+++ src/pages/ranobe.vue
@@ -7,7 +7,7 @@ defineOptions({
 
 const { t } = useI18n()
 
-const books = ref([])
+const books = ref(null)
 
 function getBooks() {
   Api.getAllBooks()
@@ -24,6 +24,9 @@ onMounted(() => {
 <template>
   <main>
     <h2>{{ t('ranobe') }}</h2>
+    <div v-if="!books" class="shelf">
+      <CardBlank v-for="l in [1, 2, 3]" :key="l" />
+    </div>
     <div id="ranobe" class="shelf">
       <CardRanobe v-for="item in books" :key="item.id" :ranobe="item" />
     </div>
