b7d6ddc70e231440d00060e7dd41648f1ab3ae32
Beaend <beaend@ya.ru>
2023-11-20 12:18:07 +0300

    add mouse position tracking

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,37 @@
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
-import { windowStore } from '~/stores/window'
+import { displayStore } from '~/stores/display'
 
-const siteWindow = windowStore()
+const display = displayStore()
 
 onMounted(() => {
   setWindowStoreSizeValue()
   window.addEventListener('resize', () => {
     setWindowStoreSizeValue()
   })
+  const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0
+  if (isTouchDevice) {
+    display.isTouch = true
+    document.addEventListener('touchstart', clickToShowBlock)
+  }
+  else {
+    document.addEventListener('mousemove', moveBlock)
+  }
 })
 
 function setWindowStoreSizeValue(): void {
-  siteWindow.width = window.innerWidth
-  siteWindow.height = window.innerHeight
+  display.window.width = window.innerWidth
+  display.window.height = window.innerHeight
+}
+
+function moveBlock(event) {
+  display.mousePosition[0] = event.clientX
+  display.mousePosition[1] = event.clientY
+}
+function clickToShowBlock(event) {
+  display.mousePosition[0] = event.touches[0].clientX
+  display.mousePosition[1] = event.touches[0].clientY
 }
 
 useHead({
